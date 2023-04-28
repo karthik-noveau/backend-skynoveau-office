3f2a7e68fec79f13184515df3a0d5d94310bd2b9
Karthik <karthik5112001@gmail.com>
2023-04-28 21:22:36 +0530

    commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
