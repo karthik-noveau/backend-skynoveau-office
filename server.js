d07c6c61578d6922cc36d0c83b2e02699236f794
Karthik <karthik5112001@gmail.com>
2023-04-28 05:48:39 +0530

    commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require("cors")
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const url = 'mongodb+srv://karthik:9HKqbiyvS4yUCnm2@cluster0.gwt9dpe.mongodb.net/SkynoveaDatabase?retryWrites=true&w=majority';
+const dbName = 'SkynoveaDatabase';
+
+const  connectToDatabase = async()=> {
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  return { client, db };
+}
+
+app.post('/feedback', async (req, res) => {
+  const data = req.body;
+  try {
+    const { client, db } = await connectToDatabase();
+    const collection = db.collection('feedback');
+    const result = await collection.insertOne(data);
+    client.close();
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.listen(8080, () => {
+  console.log('Server listening on port 8080');
+});
